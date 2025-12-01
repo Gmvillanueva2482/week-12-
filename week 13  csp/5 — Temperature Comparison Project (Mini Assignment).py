d543421b989d0e23c24f41a85e7f5d6f62d8cff3
Gmvillanueva2482 <Gmvillanueva1@cps.edu>
2025-12-01 15:05:36 +0000

    objective

--- week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
+++ week 13  csp/5 — Temperature Comparison Project (Mini Assignment).py
@@ -1,5 +1,18 @@
 # Objective:
 # Apply comparison and logical operators to a real-world problem.
+# A real world example of a comparison and longical operation in the real-wolrd otday, would be temperture and weather
+
+Weather = "rainy"
+Temp = "13" 
+
+if Weather == "rainy" and Temp < 15: 
+    print ("Wear a coat and make sure to bring an umbrella")
+elif Weather == "sunny" and Temp > 30:
+    print("Wear something cool and drink some water")
+elif Weather == "cloudy" or Temp < 10 :
+    print("Weather is a bit chilly may have to wear a coat")
+else :
+    print ("Well, the weater seems fine so just have fun ouside!!!")
 
 # Scenario:
 # Write a program that:
